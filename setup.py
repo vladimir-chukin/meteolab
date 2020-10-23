96592f5f370ea622daa7c502220e024961b7058d
Vladimir Chukin <chukin@meteolab.ru>
2020-10-23 16:03:46 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Air',
+      version='1.0',
+      description='Python Module Example',
+      author='MeteoLab',
+      author_email='hello@meteolab.org',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
